d867433b46c1cf7ee4b6ee18d939f4c295511148
Esaiy <93sultanbaney@gmail.com>
2019-12-08 14:53:59 +0700

    Cooldown fire dan mengubah list ke array

--- src/id/ac/its/fppbo/game/SpaceShip.java
+++ src/id/ac/its/fppbo/game/SpaceShip.java
@@ -9,9 +9,12 @@ public class SpaceShip extends Sprite {
 	
 	private int dx;
 	private int dy; 
-	private List<Missile> missiles;
+//	private List<Missile> missiles;
+	private Missile[] missiles;
 	private int health = 3;
 	private int MAX_HEALTH = 8;
+	public long lastMissile = 0;
+	private static int missileCount = 0;
 	
 	public SpaceShip(int x, int y) {
 		super(x,y);
@@ -20,7 +23,12 @@ public class SpaceShip extends Sprite {
 	}
 	
 	private void initSpaceShip() {
-		missiles = new ArrayList<>();
+		missiles = new Missile[10];
+		if(missileCount==0) {
+			for(int i = 0;i < 10;i++) {
+				missiles[i] = new Missile(0,0,0);
+			}
+		}
 		
 		loadImage("src/resource/SpaceShip/pesawat.png");
 		getImageDimension();
@@ -42,7 +50,7 @@ public class SpaceShip extends Sprite {
 			y = 200;
 	}
 	
-	public List<Missile> getMissiles(){
+	public Missile[] getMissiles(){
 		return missiles;
 	}
 	
@@ -74,7 +82,11 @@ public class SpaceShip extends Sprite {
 		}
 		
 		if(key == KeyEvent.VK_SPACE) {
-			fire();
+			long now = System.currentTimeMillis();
+			if(now - lastMissile > 300) {
+				fire(missileCount++);
+				lastMissile = now; 
+			}
 		}
 		//test hp
 		if(key == KeyEvent.VK_C) {
@@ -92,8 +104,11 @@ public class SpaceShip extends Sprite {
 		health-=1;
 	}
 	
-	public void fire() {
-		missiles.add(new Missile(x+width/2-6, y-height, -2));
+	public void fire(int count) {
+//		missiles.add(new Missile(x+width/2-6, y-height, -2));
+		missiles[count%20] = new Missile(x+width/2-6, y-height, -2);
+		
+		
 //		missiles.add(new Missile(x, y-height, -2));
 	}
 	
