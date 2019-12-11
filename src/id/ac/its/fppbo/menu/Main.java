8bf0a1598b863fb68b12c217906c2176522d2f4f
Juwita14 <56763570+Juwita14@users.noreply.github.com>
2019-12-11 06:39:46 -0800

    button muncul

--- src/id/ac/its/fppbo/menu/Main.java
+++ src/id/ac/its/fppbo/menu/Main.java
@@ -1,5 +1,6 @@
 package id.ac.its.fppbo.menu;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -14,30 +15,32 @@ public class Main {
 	private Image image;
 	
 	public Main() {
-		menuframe();
+		
+	JFrame frame = new JFrame();
+	frame.setSize(500, 600);
+	
+	ImageIcon img = new ImageIcon("src/id/ac/its/fppbo/player.png");
+	image = img.getImage();
+	image = image.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+	
+	JPanel panel = new JPanel();
+	JButton playbutton = new JButton("Play");
+	JButton quitbutton = new JButton("Quit");
+	JLabel label = new JLabel();
+	panel.setBackground(Color.WHITE);
+	
+	panel.add(playbutton);
+	panel.add(quitbutton);
+	panel.add(label);
+
+	frame.add(panel, BorderLayout.SOUTH);
+	
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public void menuframe() {
-		JFrame frame = new JFrame();
-		frame.setSize(500, 600);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		ImageIcon img = new ImageIcon("player.png");
-		image = img.getImage();
-		image = image.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-		
-		JPanel panel = new JPanel();
-		JButton playbutton = new JButton("Play");
-		JButton quitbutton = new JButton("Quit");
-		JLabel label = new JLabel();
-		
-		panel.add(playbutton);
-		panel.add(quitbutton);
-		panel.add(label);
-
-		frame.add(panel, BorderLayout.SOUTH);
-		
+	public Image getimage() {
+		return image;
 	}
 	
 	public static void main(String[] args) {
