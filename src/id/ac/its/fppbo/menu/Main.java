9ca92fb6b1f0d6fac0a33d4c5c806f9624553a36
Juwita14 <56763570+Juwita14@users.noreply.github.com>
2019-12-11 04:44:45 -0800

    menu baru

--- src/id/ac/its/fppbo/menu/Main.java
+++ src/id/ac/its/fppbo/menu/Main.java
@@ -1,146 +1,47 @@
 package id.ac.its.fppbo.menu;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.awt.BorderLayout;
+import java.awt.Image;
 
-import javafx.application.*;
-import javafx.stage.*;
-import javafx.geometry.*;
-import javafx.scene.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Main extends Application{
-	
-	private Parent createContent() {
-		Pane root = new Pane();
-		
-		root.setPrefSize(500, 600);
-		
-		try(InputStream is = Files.newInputStream(Paths.get("background3.jpg"))){
-			ImageView img = new ImageView(new Image(is));
-			img.setFitWidth(500);
-			img.setFitHeight(600);
-			root.getChildren().add(img);
-		}
-		catch(IOException e) {
-			System.out.println("Couldn't load image");
-			System.out.println("test");
-		}
-		
-		Title title = new Title ("S P A C E");
-		title.setTranslateX(55);
-		title.setTranslateY(150);
-		
-		MenuBox vbox = new MenuBox(
-				new MenuItem("GAME"),	
-				new MenuItem("QUIT"));
-		vbox.setTranslateX(150);
-		vbox.setTranslateY(325);
-		
-		root.getChildren().addAll(title,vbox);
-		
-		return root;
-		
-	}
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		Scene scene = new Scene(createContent());
-		primaryStage.setTitle("VIDEO GAME");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	private static class Title extends StackPane{
-		public Title(String name) {
-			Rectangle bg = new Rectangle(375, 60);
-			bg.setStroke(Color.WHITE);
-			bg.setStrokeWidth(2);
-			bg.setFill(null);
-			
-			Text text = new Text(name);
-			text.setFill(Color.WHITE);
-			text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, 50));
-			
-			setAlignment(Pos.CENTER);
-			getChildren().addAll(bg,text);
-		}
-	}
-	
-	private static class MenuBox extends VBox{
-		public MenuBox(MenuItem...items) {
-			getChildren().add(createSeperator());
-			
-			for(MenuItem item : items) {
-				getChildren().addAll(item, createSeperator());
-			}
-		}
-		
-		private Line createSeperator() {
-			Line sep = new Line();
-			sep.setEndX(210);
-			sep.setStroke(Color.WHITE);
-			return sep;
-		}
-		
-	}
-	
-	private static class MenuItem extends StackPane{
-		public MenuItem(String name) {
-			LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[] { 
-				new Stop(0, Color.WHITE),
-				new Stop(0.1, Color.BLACK),
-				new Stop(0.9, Color.BLACK),
-				new Stop(1, Color.WHITE)
-				
-			});
-			
-			Rectangle bg = new Rectangle(200,30);
-			bg.setOpacity(0.4);
-			
-			Text text = new Text(name);
-			text.setFill(Color.DARKGREY);
-			text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD,20));
-			
-			setAlignment(Pos.CENTER);
-			getChildren().addAll(bg, text);
-			setOnMouseEntered(event -> {
-				bg.setFill(gradient);
-				text.setFill(Color.WHITE);
-				
-			});
-			
-			setOnMouseExited(event -> {
-				bg.setFill(Color.BLACK);
-				text.setFill(Color.DARKGREY);
-			});
-			setOnMousePressed(event -> {
-				bg.setFill(Color.VIOLET);
-				
-			});
-			
-			setOnMouseReleased(event -> {
-				bg.setFill(gradient);
-			});
-			
-			}
-		}
 
+public class Main {
+	
+	private Image image;
+	
+	public Main() {
+		menuframe();
+	}
+	
+	public void menuframe() {
+		JFrame frame = new JFrame();
+		frame.setSize(500, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ImageIcon img = new ImageIcon("player.png");
+		image = img.getImage();
+		image = image.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+		
+		JPanel panel = new JPanel();
+		JButton playbutton = new JButton("Play");
+		JButton quitbutton = new JButton("Quit");
+		JLabel label = new JLabel();
+		
+		panel.add(playbutton);
+		panel.add(quitbutton);
+		panel.add(label);
+
+		frame.add(panel, BorderLayout.SOUTH);
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		launch(args);
+		new Main();
 	}
 }
